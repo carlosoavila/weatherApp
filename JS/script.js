e6086b8e2e6d75e63df739532f6f9ea7ae692de3
Carlos Alberto <carlosoavila1221@gmail.com>
2023-11-27 06:24:40 -0300

    start api

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const apiKey = 'a2a800b9757efb616e99d73bbad5e1fc'
+const apiCountryUrl = 'https://flagsapi.com/BE/flat/64.png'
+const city = "esteio"
+
+
+//functions
+
+const getWeatherData = async(city) => {
+  
+  const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+  const response = await fetch(apiWeatherUrl)
+  const data = await response.json()
+
+  console.log(data)
+}
+
+getWeatherData(city)
