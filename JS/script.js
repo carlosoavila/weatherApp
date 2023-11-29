96d4c5fd1341413404dc6cfbbdb4a39aeeb96c4c
Carlos Alberto <carlosoavila1221@gmail.com>
2023-11-29 06:26:40 -0300

    add weather data and icons

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
 const apiKey = 'a2a800b9757efb616e99d73bbad5e1fc'
-const apiCountryUrl = 'https://flagsapi.com/BE/flat/64.png'
+const apiCountryFlagUrl = 'https://flagsapi.com/BE/flat/64.png'
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
+const cityName = document.querySelector('.city-name')
+const cityTemperature = document.querySelector('#city-temperature')
+const cityHumidity = document.querySelector('#city-humidity')
 
 
 //functions
@@ -13,6 +16,9 @@ const getWeatherData = async(city) => {
   const response = await fetch(apiWeatherUrl)
   const data = await response.json()
 
+  cityName.innerText = data.name
+  cityTemperature.innerText = data.main.temp + " ºC"
+  cityHumidity.innerText = data.main.humidity + " Umidade"
   console.log(data)
 }
 
